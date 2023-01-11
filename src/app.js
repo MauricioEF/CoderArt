475d0874d32b259aa8a89c1ec1b13e33954680b6
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2023-01-11 07:30:24 -0600

    Added router-controller model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
+import viewsRouter from './routes/views.router.js';
+import sessionsRouter from './routes/sessions.router.js';
 
 
 const app = express();
@@ -10,8 +12,7 @@ app.engine('handlebars',handlebars.engine());
 app.set('views',`${__dirname}/views`);
 app.set('view engine','handlebars');
 
-app.get('/',(req,res)=>{
-    res.render('register');
-})
+app.use('/',viewsRouter);
+app.use('/api/sessions',sessionsRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
